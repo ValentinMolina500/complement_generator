d878c0513afc809973ffa669743f1240a5324d23
Valentin Molina <valentinmolina83@gmail.com>
2018-10-12 15:18:00 -0700

    Added compliment_generator with random index

--- main.c
+++ main.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+#define NUM_COMPLIMENTS 3
 
-void compliments_generator ()
+void compliments_generator ( int i )
 {
     struct compliments
     {
         char compliment[30];
     };
 
-    struct compliments franklin[10] =
+    struct compliments franklin[NUM_COMPLIMENTS] = {
+        {
+            .compliment = "You are beautiful!",
+        },
+        {
+            .compliment = "You have a great personality.",
+        },
+        {
+            .compliment = "You are smart!",
+        },
+    };
 
+    printf ("%s\n", franklin[i].compliment);
+    printf ("\n");
+}
+
+int main ()
+{
+    int index;
+    printf ("I have the perfect compliment for you!");
+
+    srand(time(0));
+    index = (rand() % 2) + 1;
+
+    compliments_generator(index);
+
+    return 0;
 }
