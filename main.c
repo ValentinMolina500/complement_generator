5c447a636f1c4a3843bdd97a6619b4bf14ab5b42
Valentin Molina <valentinmolina83@gmail.com>
2018-11-11 20:07:30 -0800

    Edited Compliment.txt

--- main.c
+++ main.c
@@ -2,10 +2,21 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
+#include <ctype.h>
 #define NUM_COMPLIMENTS 100
 
-void compliments_generator ( int i )
+int random (void)
 {
+    int i = 0;
+
+    srand(time(0));
+    i = (rand() % 101);
+
+    return i;
+}
+void compliments_generator (int i)
+{
+    char choice = 0;
     FILE *file;
     struct compliments
     {
@@ -21,6 +32,27 @@ void compliments_generator ( int i )
 
     printf ("%s", franklin[i].compliment);
 
+    while (choice != 'n')
+    {
+        printf ("\nWould you like another compliment? (Y/N) ");
+        scanf (" %1c", &choice);
+        choice = tolower(choice);
+        switch (choice)
+        {
+            case 'y':
+                i = random();
+                printf ("\n%s", franklin[i].compliment);
+                break;
+
+            case 'n':
+                break;
+
+            default:
+                printf ("\nPlease enter either Y or N.\n");
+                break;
+        }
+    }
+
     fclose (file);
 }
 
@@ -29,10 +61,9 @@ int main ()
     int index;
     printf ("I have the perfect compliment for you! ");
 
-    srand(time(0));
-    index = (rand() % 100) + 1;
-
+    index = random();
     compliments_generator(index);
 
+    printf ("\nSee you! Have a wonderful day!\n");
     return 0;
 }
